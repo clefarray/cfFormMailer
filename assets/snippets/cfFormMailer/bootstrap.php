5ece84f88749c6a25fd3c8c1fcd49bee2382fdff
yama <yamamoto@kyms.jp>
2019-04-27 22:19:28 +0900

    Version 1.6

--- assets/snippets/cfFormMailer/bootstrap.php
+++ assets/snippets/cfFormMailer/bootstrap.php
@@ -4,7 +4,7 @@
  * 
  * @author  Clefarray Factory
  * @link  http://www.clefarray-web.net/
- * @version 1.4
+ * @version 1.6
  *
  * Documentation: http://www.clefarray-web.net/blog/manual/cfFormMailer_manual.html
  * LICENSE: GNU General Public License (GPL) (http://www.gnu.org/copyleft/gpl.html)
