a2da9ddc5779c41afa4479cbcd6493f9dbf09357
yamamoto <yamamoto@kyms.jp>
2025-11-18 02:32:36 +0900

    ディレクトリ構成を変更

--- install-code.php
+++ install-code.php
@@ -10,4 +10,4 @@
  * LICENSE: GNU General Public License (GPL) (http://www.gnu.org/copyleft/gpl.html)
  */
 
-return include_once(MODX_BASE_PATH . 'assets/snippets/cfFormMailer/bootstrap.php');
+return include_once MODX_BASE_PATH . 'assets/snippets/cfFormMailer/includes/bootstrap.php';
