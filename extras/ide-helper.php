c91aee1b7bedf256de858c5fc6ea7aa525c1c721
yamamoto <yamamoto@kyms.jp>
2025-11-24 12:58:38 +0900

    ide-helper.phpを新規作成し、基本情報とライセンスを追加

---
+++ extras/ide-helper.php
@@ -0,0 +1,12 @@
+<?php
+define('MODX_BASE_PATH', dirname(__FILE__, 3) . '/');
+/**
+ * cfFormMailer
+ * 
+ * @author  Clefarray Factory
+ * @link  http://www.clefarray-web.net/
+ * @version 1.6
+ *
+ * Documentation: http://www.clefarray-web.net/blog/manual/cfFormMailer_manual.html
+ * LICENSE: GNU General Public License (GPL) (http://www.gnu.org/copyleft/gpl.html)
+ */
